0481a59482ddb623de19c96f4bf4e49833d2e0e7
Slava Tutrinov <vtutrinov@gmail.com>
2012-03-12 17:08:13 +0400

    Чистим ненужное

--- recreate_ulmart_rt_index.php
+++
@@ -1,6 +0,0 @@
-<?php
-$link = mysql_connect('127.0.0.1:3306', 'root', '123');
-mysql_query("SET NAMES utf8", $link);
-mysql_query("SET CHARACTER SET utf8", $link);
-mysql_select_db('ulmart', $link);
-?>
